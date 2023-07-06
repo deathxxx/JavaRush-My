ed0eb6c51123c193493deb00b88a5b6632b14d55
dex <evilz@bk.ru>
2023-07-06 09:55:35 +0600

    2511-2512-2513-2514-fix

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
@@ -11,6 +11,17 @@ public class Solution implements Thread.UncaughtExceptionHandler {
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
+        t.interrupt();
+
+        List<String> res = new LinkedList<>();
+        while (e != null) {
+            res.add(0, e.toString());
+            e = e.getCause();
+        }
+
+        for (String s : res) {
+            System.out.println(s);
+        }
     }
 
     public static void main(String[] args) {
