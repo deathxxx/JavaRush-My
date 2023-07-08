050802c3a8df24b6968cf87d4fd2bd3a45af3b79
dex <evilz@bk.ru>
2023-07-08 10:27:01 +0600

    2811 - fix

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2811/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2811/Solution.java
@@ -7,7 +7,7 @@ ReentrantReadWriteLock
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ReadWriteMap<Integer, String> linkedSafeMap = new ReadWriteMap<>(new LinkedHashMap<>());
     }
 }
