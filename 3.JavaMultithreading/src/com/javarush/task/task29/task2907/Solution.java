8e09fe43984b655229c5a826e70163a854763d9b
evgeniy.syrygin@auslogics.com <evgeniy.syrygin@auslogics.com>
2024-02-29 11:28:44 +0600

    2907-fix

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2907/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2907/Solution.java
@@ -8,10 +8,12 @@ import java.math.BigDecimal;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(getValue(1.1d, 1.2d));
+        System.out.println(getValue(1.1d, 1.2d));    //2.3 expected
+        System.out.println(getValue(-1.1d, 1.1d));   //0.0 expected
+        System.out.println(getValue(.100d, 1.100d)); //1.2 expected
     }
 
     public static BigDecimal getValue(double v1, double v2) {
-        return new BigDecimal(v1).add(new BigDecimal(v2));
+        return new BigDecimal(String.valueOf(v1)).add(new BigDecimal(String.valueOf(v2)));
     }
 }
