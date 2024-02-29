cb903b08e2aef351d270558e76871764f901b759
evgeniy.syrygin@auslogics.com <evgeniy.syrygin@auslogics.com>
2024-02-29 11:10:09 +0600

    2901-2

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2901/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2901/Solution.java
@@ -12,41 +12,41 @@ import java.util.List;
 */
 
 public class Solution {
-    public static final String defaultFileName = "C:/tmp/strange_file_name.tmp";
+    public static final String DEFAULT_FILE_NAME = "C:/tmp/strange_file_name.tmp";
 
     private final String localFileName;
-    private List<String> contentAslines;
+    private List<String> contentAsLines;
     private boolean fileLoaded;
 
     public Solution(String firstFileName) {
-        localFileName = firstFileName == null ? defaultFileName : firstFileName;
+        localFileName = firstFileName == null ? DEFAULT_FILE_NAME : firstFileName;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         String fileName = Solution.class.getResource("Solution.java").getPath();
 
         Solution solution = new Solution(fileName);
-        solution.DownloadFileContent();
-        if (solution.getFileLoaded()) {
-            System.out.println(solution.isexpectedline("public class Solution {"));   // Expected true
-            System.out.println(solution.isexpectedline(" public class Solution {"));  // Expected false
+        solution.downloadFileContent();
+        if (solution.isFileLoaded()) {
+            System.out.println(solution.hasExpectedLine("public class Solution {"));   //expected true
+            System.out.println(solution.hasExpectedLine(" public class Solution {"));  //expected false
         }
     }
 
-    public boolean getFileLoaded() {
+    public boolean isFileLoaded() {
         return fileLoaded;
     }
 
-    public void DownloadFileContent() {
+    public void downloadFileContent() {
         try {
-            contentAslines = Files.readAllLines((new File(localFileName)).toPath(), Charset.defaultCharset());
+            contentAsLines = Files.readAllLines((new File(localFileName)).toPath(), Charset.defaultCharset());
             fileLoaded = true;
         } catch (IOException e) {
             System.out.println("Unsuccessful. What a surprise!");
         }
     }
 
-    public boolean isexpectedline(String expectedline) {
-        return contentAslines.contains(expectedline);
+    public boolean hasExpectedLine(String expectedLine) {
+        return contentAsLines.contains(expectedLine);
     }
 }
