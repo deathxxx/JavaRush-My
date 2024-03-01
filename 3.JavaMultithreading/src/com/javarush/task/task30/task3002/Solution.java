12be5a1b99dd332c8b041b20c178113762525462
evgeniy.syrygin@auslogics.com <evgeniy.syrygin@auslogics.com>
2024-03-01 10:12:55 +0600

    3002-fix

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3002/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3002/Solution.java
@@ -7,14 +7,25 @@ package com.javarush.task.task30.task3002;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(convertToDecimalSystem("0x16")); //22
-        System.out.println(convertToDecimalSystem("012"));  //10
-        System.out.println(convertToDecimalSystem("0b10")); //2
-        System.out.println(convertToDecimalSystem("62"));   //62
+        System.out.println(convertToDecimalSystem("0x16"));
+        System.out.println(convertToDecimalSystem("012"));
+        System.out.println(convertToDecimalSystem("0b10"));
+        System.out.println(convertToDecimalSystem("62"));
     }
 
     public static String convertToDecimalSystem(String s) {
-        //напишите тут ваш код
-        return s;
+        if (s.startsWith("0")) {
+            char[] chars = s.toCharArray();
+            if (chars[1] == 'b') {
+                s = s.substring(2);
+                return "" + Integer.parseInt(s, 2);
+            } else if (chars[1] == 'x') {
+                s = s.substring(2);
+                return "" + Integer.parseInt(s, 16);
+            }
+            return "" + Integer.parseInt(s, 8);
+        } else {
+            return s;
+        }
     }
 }
