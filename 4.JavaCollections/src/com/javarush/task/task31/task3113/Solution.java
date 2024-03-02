a8c8c4b12ed3f083dd119fe51be20b4ebcc1fc54
evgeniy.syrygin@auslogics.com <evgeniy.syrygin@auslogics.com>
2024-03-02 13:01:46 +0600

    3113-fix

--- 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -15,5 +15,37 @@ import java.util.concurrent.atomic.AtomicLong;
 public class Solution {
 
     public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String dirName = reader.readLine();
+
+        Path directory = Paths.get(dirName);
+
+        if (!Files.isDirectory(directory)) {
+            System.out.println(directory.toString() + " - не папка.");
+        } else {
+            AtomicInteger folderCounter = new AtomicInteger();
+            AtomicInteger fileCounter = new AtomicInteger();
+            AtomicLong sizeCounter = new AtomicLong();
+
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    if (!dir.equals(directory)) folderCounter.incrementAndGet();
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    fileCounter.incrementAndGet();
+                    sizeCounter.addAndGet(attrs.size());
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+            System.out.println("Параметры папки " + directory.toString() + ":");
+            System.out.println("Всего папок - " + folderCounter.get());
+            System.out.println("Всего файлов - " + fileCounter.get());
+            System.out.println("Общий размер - " + sizeCounter.get() + " байт");
+        }
     }
 }
