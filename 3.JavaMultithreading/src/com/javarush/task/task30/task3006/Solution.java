f4694b0db134e356677ecf3d244cb8811f8f2e21
Evgeniy <evgeniy.syrygin@auslogics.com>
2024-11-09 20:13:41 +0600

    3006-fix

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3006/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3006/Solution.java
@@ -6,11 +6,6 @@ Swap по-новому
 
 public class Solution {
     public static void main(String[] args) {
-        /* expected output
-        x=4, y=5
-        x=5, y=4
-         */
-
         Pair pair = new Pair(4, 5);
         System.out.println(pair);
         pair.swap();
