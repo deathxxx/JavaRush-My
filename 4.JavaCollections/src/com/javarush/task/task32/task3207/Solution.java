83062f43d4eb8aa1458bd55bf23510352a23e6dc
Evgeniy <evgeniy.syrygin@auslogics.com>
2024-11-10 08:49:07 +0600

    3207-fix

--- 4.JavaCollections/src/com/javarush/task/task32/task3207/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3207/Solution.java
@@ -16,16 +16,24 @@ public class Solution {
     public static final String UNIC_BINDING_NAME = "double.string";
     public static Registry registry;
 
-    // Pretend we're starting an RMI client as the CLIENT_THREAD thread
+    //pretend we start rmi client as CLIENT_THREAD thread
     public static Thread CLIENT_THREAD = new Thread(new Runnable() {
         @Override
         public void run() {
-            //напишите тут ваш код
+            try {
+                DoubleString service = (DoubleString) registry.lookup(UNIC_BINDING_NAME);
+                String result = service.doubleString("test string");
+                System.out.println(result);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            } catch (NotBoundException e) {
+                e.printStackTrace();
+            }
         }
     });
 
     public static void main(String[] args) {
-        // Pretend we're starting an RMI server as the main thread
+        //pretend we start rmi server as main thread
         Remote stub = null;
         try {
             registry = LocateRegistry.createRegistry(2099);
@@ -39,7 +47,7 @@ public class Solution {
             e.printStackTrace();
         }
 
-        // Start the client
+        //start client
         CLIENT_THREAD.start();
     }
 }
