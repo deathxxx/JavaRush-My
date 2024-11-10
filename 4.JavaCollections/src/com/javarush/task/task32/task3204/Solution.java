80c9963654f55e20fac51d2fa4235a51f8ce167c
Evgeniy <evgeniy.syrygin@auslogics.com>
2024-11-10 08:38:25 +0600

    3204-fix

--- 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3204/Solution.java
@@ -9,10 +9,49 @@ import java.io.ByteArrayOutputStream;
 public class Solution {
     public static void main(String[] args) {
         ByteArrayOutputStream password = getPassword();
-        System.out.println(password.toString());
+        System.out.println(password);
     }
 
     public static ByteArrayOutputStream getPassword() {
-        return null;
+        ByteArrayOutputStream is = new ByteArrayOutputStream();
+        Basket[] baskets = new Basket[3];
+        baskets[0] = new Basket('0', '9');
+        baskets[1] = new Basket('a', 'z');
+        baskets[2] = new Basket('A', 'Z');
+        int i = 0;
+        for (; i < 5; i++) {
+            int index = (int) (Math.random() * 3);
+            generateChar(is, baskets, index);
+        }
+        for (int j = 0; j < baskets.length; j++) {
+            generateChar(is, baskets, j);
+            i++;
+        }
+
+        while (i < 8) {
+            int index = (int) (Math.random() * 3);
+            generateChar(is, baskets, index);
+            i++;
+        }
+        return is;
+    }
+
+    private static void generateChar(ByteArrayOutputStream is, Basket[] baskets, int index) {
+        Basket basket = baskets[index];
+        is.write((char) basket.getRandom());
+    }
+
+    public static class Basket {
+        int begin;
+        int quantity;
+
+        private Basket(char begin, char end) {
+            this.begin = begin;
+            this.quantity = end - begin + 1;
+        }
+
+        public int getRandom() {
+            return (int) (Math.random() * quantity) + begin;
+        }
     }
 }
