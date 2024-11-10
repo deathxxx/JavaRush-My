72506d47b09cc80499375e318cbf4a79ad8053a1
Evgeniy <evgeniy.syrygin@auslogics.com>
2024-11-10 08:20:46 +0600

    3201-fix

--- 4.JavaCollections/src/com/javarush/task/task32/task3201/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3201/Solution.java
@@ -9,6 +9,14 @@ import java.io.RandomAccessFile;
 
 public class Solution {
     public static void main(String... args) {
-
+        try (RandomAccessFile raf = new RandomAccessFile(args[0], "rw")) {
+            long number = Long.parseLong(args[1]);
+            long length = raf.length();
+            number = number > length ? length : number;
+            raf.seek(number);
+            raf.write(args[2].getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
