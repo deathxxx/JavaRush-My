9a085b3058b57c548680021d7756477ec87fe21c
Evgeniy <evgeniy.syrygin@auslogics.com>
2024-11-10 08:22:41 +0600

    3210-fix

--- 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
@@ -9,6 +9,25 @@ import java.io.RandomAccessFile;
 
 public class Solution {
     public static void main(String... args) {
+        try (RandomAccessFile raf = new RandomAccessFile(args[0], "rw")) {
+            long number = Long.parseLong(args[1]);
+            String text = args[2];
+            long fileLength = raf.length();
 
+            raf.seek(number);
+            byte[] readBytes = new byte[text.length()];
+            raf.read(readBytes, 0, text.length());
+
+            String readString = new String(readBytes);
+
+            raf.seek(fileLength);
+            if (readString.equals(text)) {
+                raf.write("true".getBytes());
+            } else {
+                raf.write("false".getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
