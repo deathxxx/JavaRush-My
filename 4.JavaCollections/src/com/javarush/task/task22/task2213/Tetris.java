f5a2c556c6fdb350de52f963bec7c60e6b5b0a57
Evgeniy <evgeniy.syrygin@auslogics.com>
2024-11-10 10:12:33 +0600

    2213-16

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -77,14 +77,18 @@ public class Tetris {
     }
 
     public void step() {
-        //опускаем фигурку вниз
+        //опускам фигурку вниз
         figure.down();
 
         //если разместить фигурку на текущем месте невозможно
         if (!figure.isCurrentPositionAvailable()) {
             figure.up();                    //поднимаем обратно
             figure.landed();                //приземляем
+
+            isGameOver = figure.getY() <= 1;//если фигурка приземлилась на самом верху - игра окончена
+
             field.removeFullLines();        //удаляем заполненные линии
+
             figure = FigureFactory.createRandomFigure(field.getWidth() / 2, 0); //создаем новую фигурку
         }
     }
