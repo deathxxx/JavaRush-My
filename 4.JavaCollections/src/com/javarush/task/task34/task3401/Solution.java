a0bc54e6478e5e196d56489d425af83788c232f5
Evgeniy <evgeniy.syrygin@auslogics.com>
2024-11-11 08:46:39 +0600

    3401-fix 3402

--- 4.JavaCollections/src/com/javarush/task/task34/task3401/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3401/Solution.java
@@ -15,6 +15,8 @@ public class Solution {
     }
 
     public int fibonacci(int n) {
-        return 0;
+        if (n == 1 || n == 2)
+            return 1;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
