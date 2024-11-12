aa180748f0e7df5247ed110a327d804ac0cfe1ed
Evgeniy <evgeniy.syrygin@auslogics.com>
2024-11-12 07:54:43 +0600

    3502-fix

--- 4.JavaCollections/src/com/javarush/task/task35/task3502/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3502/Solution.java
@@ -6,8 +6,8 @@ import java.util.List;
 Знакомство с дженериками
 */
 
-public class Solution {
-    public static class SomeClass {
+public class Solution<T extends List<Solution.SomeClass>> {
+    public static class SomeClass<K extends Number> {
     }
 
     public static void main(String[] args) {
