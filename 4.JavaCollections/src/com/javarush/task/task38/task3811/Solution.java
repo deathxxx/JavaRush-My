c15947ac1d84960c45866221a0aa94990f7d85bc
Evgeniy <evgeniy.syrygin@auslogics.com>
2024-11-15 22:59:05 +0600

    3811-fix

--- 4.JavaCollections/src/com/javarush/task/task38/task3811/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3811/Solution.java
@@ -1,14 +1,15 @@
 package com.javarush.task.task38.task3811;
 
+@Ticket(
+        priority = Ticket.Priority.HIGH,
+        createdBy = "Noodles",
+        tags = {"bug", "asap"}
+)
+
 /* 
 Тикеты
 */
 
-@Ticket(
-        priority = Ticket.Priority.HIGH,
-        createdBy = "Noodles",
-        tags = {"bug", "fix asap"}
-)
 public class Solution {
     public static void main(String[] args) {
 
