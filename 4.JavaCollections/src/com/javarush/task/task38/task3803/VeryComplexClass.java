3ac5e59d05bf8207c218e67288f1d28e80e628c1
Evgeniy <evgeniy.syrygin@auslogics.com>
2024-11-15 22:45:15 +0600

    3803-fix

--- 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
@@ -10,11 +10,13 @@ Runtime исключения (unchecked exception)
 
 public class VeryComplexClass {
     public void methodThrowsClassCastException() {
-
+        List<Integer> list = new ArrayList<Integer>();
+        ((LinkedList<Integer>) list).add(3);
     }
 
     public void methodThrowsNullPointerException() {
-
+        int[] arr = null;
+        int i = arr.length;
     }
 
     public static void main(String[] args) {
