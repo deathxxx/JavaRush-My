61280d0f74600271c6ea41f5006d779e5a5ef876
Evgeniy <evgeniy.syrygin@auslogics.com>
2024-11-15 22:50:13 +0600

    3806-fix

--- 4.JavaCollections/src/com/javarush/task/task38/task3806/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3806/Solution.java
@@ -1,52 +1,30 @@
 package com.javarush.task.task38.task3806;
 
-/* 
-Улучшения в Java 7 (try-with-resources)
-*/
-
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/* 
+Улучшения в Java 7 (try-with-resources)
+*/
+
 public class Solution {
     public void printFile1() throws IOException {
-        FileInputStream fileInputStream = null;
-
-        try {
-            fileInputStream = new FileInputStream("file.txt");
-
+        try (FileInputStream fileInputStream = new FileInputStream("file.txt")) {
             int data = fileInputStream.read();
             while (data != -1) {
                 System.out.println(data);
                 data = fileInputStream.read();
             }
-        } finally {
-            if (fileInputStream != null) {
-                fileInputStream.close();
-            }
         }
     }
 
     public void printFile2() throws IOException {
-        FileInputStream fileInputStream = null;
-        BufferedInputStream bufferedInputStream = null;
-        try {
-            fileInputStream = new FileInputStream("file.txt");
-            bufferedInputStream = new BufferedInputStream(fileInputStream);
-
-
+        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("file.txt"))) {
             int data = bufferedInputStream.read();
             while (data != -1) {
                 System.out.println(data);
                 data = bufferedInputStream.read();
-            }
-        } finally {
-            if (fileInputStream != null) {
-                fileInputStream.close();
-            }
-
-            if (bufferedInputStream != null) {
-                bufferedInputStream.close();
             }
         }
     }
