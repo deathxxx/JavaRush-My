048bb5378ba28dd2a5df967897328bcae1446fc2
Evgeniy <evgeniy.syrygin@auslogics.com>
2025-01-29 09:43:04 +0600

    racer-game-11-f

--- 8.Games/src/com/javarush/games/racer/road/RoadObject.java
+++ 8.Games/src/com/javarush/games/racer/road/RoadObject.java
@@ -1,7 +1,7 @@
 package com.javarush.games.racer.road;
 
-import com.javarush.games.racer.GameObject;
 import com.javarush.games.racer.ShapeMatrix;
+import com.javarush.games.racer.GameObject;
 
 public class RoadObject extends GameObject {
     public RoadObjectType type;
@@ -15,20 +15,10 @@ public class RoadObject extends GameObject {
         this.height = matrix.length;
     }
 
-    /**
-     * Метод, отвечающий за передвижение препятствия. У препятствия может быть своя скорость и дополнительная,
-     * которая зависит от скорости движения игрока.
-     */
     public void move(int boost) {
         this.y += boost;
     }
 
-    /**
-     * Проверяет текущий объект и объект, который пришел в качестве параметра, на пересечение их изображений
-     * с учетом дистанции distance.
-     * Например, если в качестве distance передать число 12, а 2 объекта расположены друг от друга на расстоянии меньшем,
-     * чем 12 ячеек игрового поля, метод вернет true. В ином случае вернет false.
-     */
     public boolean isCollisionWithDistance(RoadObject roadObject, int distance) {
         if ((x - distance > roadObject.x + roadObject.width) || (x + width + distance < roadObject.x)) {
             return false;
@@ -41,9 +31,6 @@ public class RoadObject extends GameObject {
         return true;
     }
 
-    /**
-     * Возвращает матрицу изображения объекта в зависимости от его типа.
-     */
     private static int[][] getMatrix(RoadObjectType type) {
         switch (type) {
             case CAR:
@@ -61,9 +48,6 @@ public class RoadObject extends GameObject {
         }
     }
 
-    /**
-     * Возвращает высоту объекта.
-     */
     public static int getHeight(RoadObjectType type) {
         return getMatrix(type).length;
     }
